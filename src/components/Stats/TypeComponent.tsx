12641786bcd1681a684cf79f2b809e13312570b4
JosephPoncini <jxponcini@gmail.com>
2024-03-17 07:36:15 -0700

    small margin fix

--- src/components/Stats/TypeComponent.tsx
+++ src/components/Stats/TypeComponent.tsx
@@ -7,7 +7,7 @@ const TypeComponent = (props : TypeProp) => {
 
     return (
         <div id="typeContainer"
-            className="w-[70%] ml-4 flex flex-col items-baseline md:flex-row space-y-4 ^ * md:space-y-0 ^ *">
+            className="w-[70%] flex flex-col items-baseline md:flex-row space-y-4 ^ * md:space-y-0 ^ *">
                 {props.elements.map( x => {
                     return(
                         <img key={x.elementKey} className="h-[30px]" src={x.elementUrl} alt={x.element} />
